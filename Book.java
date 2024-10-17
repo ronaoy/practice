1509d8f26f5562d3e8d716574de3c787fb30bdc6
ronaoy <rbfbcthfy6ragaxzggm8@icloud.com>
2024-10-17 13:25:46 +0900

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,8 @@
+public class Book extends TangibleAssit{
+private String isbn;
+public Book(String name,int price,String isbn){
+super(name,proce,color);
+this.isbn=isbn;
+}
+public String getIsbn(){return this.isbn}
+}
